6c96ec24ea18e6f53fba99d360bc199093acfb7b
Daniel Haehn <haehn@mpsych.org>
2023-08-21 15:55:10 -0400

    Add version print.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 
 import {Boostlet} from "./boostlet.js"
 
+window.console.log('BOOSTLET VERSION 0.1-alpha');
+
 // register global namespace with a new boostlet instance
 // later we might want to support multiple active boostlets
 window.Boostlet = new Boostlet();
