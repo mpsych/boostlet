489b0c0e1d82344678c5847a915f6c6165c18023
Shruti Varade <shrutivarade27@gmail.com>
2023-08-25 06:08:10 -0400

    Adding files for OepnSeaDragon

---
+++ src/frameworks/openseadragon.js
@@ -0,0 +1,43 @@
+import {Framework} from '../framework.js';
+
+import {Util} from '../util.js';
+
+export class OpenSeaDragon extends Framework {
+    constructor(instance) {
+        super(instance);
+        this.name = 'opensedragon';
+
+    }
+
+    get_image(from_canvas) {
+    
+
+        // TODO needs to be generic and executed with Boostlet.init
+        let canvas = viewer.canvas.children[0];
+        let ctx = canvas.getContext("2d");
+
+        image = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        rgba_image = Util.rgba_to_grayscale(image.data);
+
+        return {'data':rgba_image, 'width':image.width, 'height':imgae.height};
+
+    }
+
+    set_image(new_pixels) {
+
+        new_image = new ImageData(new Uint8ClampedArray(grayscale_to_rgba(new_pixels)), image.width, image.height);
+
+        ctx.putImageData(new_image, 0, 0);
+
+    }
+
+    set_mask(new_mask) {
+
+    }
+
+    select_box(callback) {
+
+    }
+
+}
+  
