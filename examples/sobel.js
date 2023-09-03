1a10e29367dd0ff1852221bc766d74a5338b6ee4
Shruti Varade <shrutivarade27@gmail.com>
2023-09-03 16:31:46 -0400

    add script.src

--- examples/sobel.js
+++ examples/sobel.js
@@ -2,6 +2,7 @@ script = document.createElement("script");
 script.type = "text/javascript";
 // script.src = "https://boostlet.org/dist/boostlet.min.js";
 script.src = "https://shrutivarade.github.io/boostlet/dist/boostlet.min.js";
+// script.src = "https://localhost:8000/dist/boostlet.min.js";
 script.onload = run;
 document.head.appendChild(script);
 eval(script);
