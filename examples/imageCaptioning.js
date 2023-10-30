0192a2f387d9c4b95610c568300db7484cfef792
gaiborjosue <gaiborjimenezjosue@gmail.com>
2023-10-30 18:15:17 -0400

    fix

--- examples/imageCaptioning.js
+++ examples/imageCaptioning.js
@@ -5,32 +5,27 @@ script.onload = run;
 document.head.appendChild(script);
 eval(script);
 
-function run() {
+
+const API_url = "https://api-inference.huggingface.co/models/Salesforce/blip-image-captioning-large"
+const API_TOKEN = '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'
+
+
+async function run() {
     Boostlet.init();
     
     console.log("Started")
-    // print the data of the function
-    console.log(request.requestdata)
+    
+
+    const data = Boostlet.get_image();
+
+    const response = await fetch(API_url, {
+      headers: { Authorization: `Bearer ${API_TOKEN}` },
+      method: "POST",
+      body: data,
+    });
+
+    const requestdata = await response.json();
+    console.log(requestdata)
+
     console.log("finished")
   }
-
-async function request() {
-        
-    const API_url = "https://api-inference.huggingface.co/models/Salesforce/blip-image-captioning-large"
-    const API_TOKEN = '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'
-    
-    const data = Boostlet.get_image();
-  
-    try {
-      const response = await fetch(API_url, {
-        headers: { Authorization: `Bearer ${API_TOKEN}` },
-        method: "POST",
-        body: data,
-      });
-  
-       const requestdata = await response.json();
-
-    } catch (error) {
-
-    }
-}
