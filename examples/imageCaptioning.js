56dfc42b0d92124fda2040f1d05851c98511b239
Mahsa Geshvadi <mahsa@Mahsas-MacBook-Air.local>
2023-11-03 11:58:52 -0400

    cleaning the code

--- examples/imageCaptioning.js
+++ examples/imageCaptioning.js
@@ -5,41 +5,37 @@ script.onload = run;
 document.head.appendChild(script);
 eval(script);
 
-
 const API_url = "https://api-inference.huggingface.co/models/Salesforce/blip-image-captioning-large"
 const API_TOKEN = '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'
 
-
 async function run() {
+
+    // detect visualization framework
     Boostlet.init();
 
-    console.log("Started")
-
-    // Call request function and wait for it to finish
     const requestData = await request();
-
     console.log(requestData);
-    displayText(requestData)
-    console.log("finished")
+
+    // display the image captioning text
+    displayText(requestData);
+
   }
 
 async function request() {
+
   image = Boostlet.get_image(true); // grab image from canvas
   pixels = image.data;
   width = image.width;
   height = image.height;
   png_image = Boostlet.convert_to_png(pixels, width, height);
-
   const response = await fetch(API_url, {
     headers: { Authorization: `Bearer ${API_TOKEN}` },
     method: "POST",
     body: png_image,
   });
+  
+  return await response.json();
 
-  console.log("Fetched data")
-
-  const requestdata = await response.json();
-  return requestdata;
 }
 
 function displayText(requestData){
@@ -56,7 +52,5 @@ function displayText(requestData){
     window.document.body.removeChild(container);
   }
     window.document.body.appendChild(container);
-
-
 
 }
