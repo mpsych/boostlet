c07a181ba4a142d9feacbdbd20a6585c2e66c3a3
Daniel Haehn <haehn@mpsych.org>
2023-11-04 19:13:37 -0400

    Bump to beta.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 import {Boostlet} from "./boostlet.js"
 
-window.console.log('BOOSTLET VERSION 0.1-alpha');
+window.console.log('BOOSTLET VERSION 0.1-beta');
 
 // register global namespace with a new boostlet instance
 // later we might want to support multiple active boostlets
