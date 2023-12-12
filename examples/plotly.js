924b75b94808dcb80b14741fc626e641717db082
gaiborjosue <gaiborjimenezjosue@gmail.com>
2023-12-12 03:49:01 -0500

    fix(examples): Fixed mini callback on code

--- examples/plotly.js
+++ examples/plotly.js
@@ -1,8 +1,6 @@
 script = document.createElement("script");
 script.type = "text/javascript";
-// script.src = "https://boostlet.org/dist/boostlet.min.js";
-// script.src = "http://127.0.0.1:5500/boostlet-edward/dist/boostlet.min.js"
-script.src = "https://gaiborjosue.github.io/boostlet/dist/boostlet.min.js"
+script.src = "https://boostlet.org/dist/boostlet.min.js";
 
 script.onload = run;
 document.head.appendChild(script);
