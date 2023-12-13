b1872a4798816a0861bc24f1c17d9d57e2bd40fd
Daniel Haehn <haehn@mpsych.org>
2023-12-12 23:29:42 -0500

    Add dropzone.

--- examples/trako.js
+++ examples/trako.js
@@ -1,7 +1,3 @@
-
-TKOFILE = 'https://bostongfx.github.io/TRAKO/DATA/example.tko';
-
-
 script = document.createElement("script");
 script.type = "text/javascript";
 script.src = "https://boostlet.org/dist/boostlet.min.js";
@@ -17,8 +13,8 @@ async function run() {
   // load example fiber (very small and fast)
   // we need it as a NVMesh template
   nvmesh = await niivue.addMeshFromUrl({'url':'https://niivue.github.io/niivue/images/colby.trk'})
-
   
+
   // detect visualization framework
   Boostlet.init();
 
@@ -46,35 +42,95 @@ function lib_load_callback() {
 
     console.log('all libs loaded');
 
-    // we can now use the XTK TKO reader
-    xtkTrakoReader.read(TKOFILE, function(fibers) {
+    container = window.document.createElement('div');
+    container.id = 'dropzone';
+    container.style.position = 'absolute';
+    container.style.top = '10px';
+    container.style.left = '10px';
+    container.style.width = '300px';
+    container.style.height = '100px';
+    container.style.zIndex = '10000';
+    container.style.padding = '20px';
+    container.style.color = 'white';
+    container.style.background = 'black';
+    container.innerHTML = 'Drop your TKO file here!<br><br><img src="https://bostongfx.github.io/TRAKO/WEB/gfx/tko.png">';
+    window.document.body.appendChild(container);
+    container.addEventListener("drop", onDrop, false);
+    container.addEventListener("dragover", function(e) {
 
-      // now we have an X.fibers object with all points
-      nvmesh.pts = fibers.points.da;
+      container.style.color = 'red'; 
+      e.preventDefault();
 
-      number_of_fibs = fibers.points.da.length / 3 / 2;
-
-      offsets = [];
-      for (var i = 0; i < number_of_fibs; i += 2) {
-
-        offsets.push(i);
-
-      }
-
-      nvmesh.offsetPt0 = new Uint32Array(offsets);
-
-      nvmesh.fiberLength = 0.001;
-      nvmesh.fiberLengths = null;
-
-      // this sets up our NVMesh for the TRAKO data
-      nvmesh.updateFibers(niivue.gl);
-
-      // and repaints everything!
-      niivue.updateGLVolume()
-
-    });
+    }, false);
 
   }
   
 }
 
+function onDrop(e) {
+
+    e.stopPropagation();
+    e.preventDefault();
+
+    console.log('file dropped!');
+
+
+
+    var files = e.dataTransfer.files;
+
+    for ( var i = 0; i < files.length; i++) {
+
+      var f = files[i];
+      var _fileName = f.name;
+      var _fileExtension = _fileName.split('.').pop().toUpperCase();
+
+      if (_fileExtension != 'TKO') {
+
+        console.log('File format invalid!');
+        return;
+
+      }
+
+      var reader = new FileReader();
+
+      reader.onload = function(e) {
+
+        console.log(e);
+
+        var r = new xtkTrakoReader();
+        fibers = r.parse(JSON.parse(e.target.result));
+
+        // now we have an X.fibers object with all points
+        nvmesh.pts = fibers.points.da;
+
+        number_of_fibs = fibers.points.da.length / 3 / 2;
+
+        offsets = [];
+        for (var i = 0; i < number_of_fibs; i += 2) {
+
+          offsets.push(i);
+
+        }
+
+        nvmesh.offsetPt0 = new Uint32Array(offsets);
+
+        nvmesh.fiberLength = 0.001;
+        nvmesh.fiberLengths = null;
+
+        // this sets up our NVMesh for the TRAKO data
+        nvmesh.updateFibers(niivue.gl);
+
+        // and repaints everything!
+        niivue.updateGLVolume()
+
+        // hide the container
+        window.document.body.removeChild(container);
+
+
+      };
+
+      reader.readAsText(f);
+
+    }
+
+}
