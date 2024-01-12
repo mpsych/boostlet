9f06860af20aedd2954844e388475f76103d76c4
gaiborjosue <gaiborjimenezjosue@gmail.com>
2024-01-12 12:49:30 -0500

    test

--- tests/scripts/index.js
+++ tests/scripts/index.js
@@ -108,7 +108,7 @@ const runItAll = async (config) => {
         const imagePath = path.join(imagesDir, file);
         const image = fs.readFileSync(imagePath)
         const image64 = Buffer.from(image).toString('base64');
-        summary.addRaw(`![Hello World](data:image/png;base64,${image64})`, true);
+        summary.addImage(`data:image/png;base64,${image64}`, 'SS Testing', { width: '800', height: '1200' });
       }
     });
 
