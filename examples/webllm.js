fc47d3082cf63f684d24709de994a061bf91d302
gaiborjosue <gaiborjimenezjosue@gmail.com>
2024-05-10 19:54:17 -0400

    feat(web-llm): Beta version working

--- examples/webllm.js
+++ examples/webllm.js
@@ -8,32 +8,26 @@ document.head.appendChild(script);
 eval(script);
 
 async function run() {
-    // detect visualization framework
-    Boostlet.init();
-  
-    // Dynamically load the WebLLM script
-    const script = document.createElement('script');
-    // It is a module script to avoid conflicts with the global scope
-    script.type = 'module';
-    script.src = 'https://raw.githubusercontent.com/mpsych/cdn/main/web-llm/index.js';
-    document.head.appendChild(script);
-  
-    // Wait for the script to load
-    script.onload = async () => {
-      // Import the module
-      const module = await import(script.src);
-      // Now you can use the exported items from the module
-      // For example, if the module exports a function called `init`, you can call it like this:
-      console.log(module);
-      window.webllm = module;
-      setupChatUI();
-      populateModelSelector();
-    };
-  }
+  // detect visualization framework
+  Boostlet.init();
+
+  const script = document.createElement("script");
+  script.type = "module";
+  script.src = "https://mpsych.github.io/cdn/web-llm/web-llm-cdn.js";
+  document.head.appendChild(script);
+
+  script.onload = async () => {
+    // Import the module
+    const module = await import(script.src);
+    window.webllm = module;
+    setupChatUI();
+    populateModelSelector();
+  };
+}
 
 function setupChatUI() {
   const style = document.createElement("style");
-  style.type = 'text/css';
+  style.type = "text/css";
   style.innerHTML = `
     .chatui {
         display: flex;
@@ -43,10 +37,11 @@ function setupChatUI() {
         flex-direction: column;
         width: 350px;
         height: 600px;
-        border: 2px solid #ddd;
-        border-radius: 5px;
+        border: 2px solid;
+        border-radius: 12px;
         background-color: #1F2027;
         z-index: 1000;
+        padding: 10px;
     }
     .chatui-select-wrapper {
         display: flex;
@@ -58,7 +53,9 @@ function setupChatUI() {
         width: 90%;
         background-color: #1F2027;
         color: white;
-        border: none;
+        height: 70%;
+        align-self: center;
+        border-radius: 5px;
     }
     #chatui-select:focus {
         outline: none;
@@ -94,7 +91,7 @@ function setupChatUI() {
         margin-left: 10px;
     }
     .chatui-send-btn:hover, .chatui-reset-btn:hover {
-        background-color: #03a33e;
+        background-color: #007EC6;
     }
     .msg-bubble {
         display: flex;
@@ -126,15 +123,23 @@ function setupChatUI() {
         color: #ccc;
         text-align: center;
     }
+    .select-label {
+        font-size: 16px;
+        color: #ccc;
+        text-align: left;
+    }
+
+    
   `;
   document.head.appendChild(style);
 
-  const chatContainer = document.createElement('div');
-  chatContainer.className = 'chatui';
+  const chatContainer = document.createElement("div");
+  chatContainer.className = "chatui";
   document.body.appendChild(chatContainer);
 
   chatContainer.innerHTML = `
       <div class="chatui-select-wrapper">
+          <p class="select-label">Select a model to start chatting: </p>
           <select id="chatui-select">
               <option selected>Select Model...</option>
           </select>
@@ -146,85 +151,103 @@ function setupChatUI() {
           <button id="chatui-reset-btn" class="chatui-reset-btn">Reset</button>
       </div>
   `;
+
+  const chatInput = document.getElementById("chatui-input");
+  chatInput.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+      event.preventDefault(); // Prevent the default action to avoid submitting the form
+      document.getElementById("chatui-send-btn").click(); // Trigger the click event on the send button
+    }
+  });
+
+  Boostlet.hint("Models with “-1k” suffix signify 1024 context length, lowering ~2-3GB VRAM requirement compared to their counterparts. Feel free to start trying with those", 5000);
 }
 
 function populateModelSelector() {
-    const modelSelect = document.getElementById('chatui-select');
-    webllm.prebuiltAppConfig.model_list.forEach((model, index) => {
-        let option = new Option(model.model_id, model.model_id);
-        modelSelect.add(option);
-    });
+  const modelSelect = document.getElementById("chatui-select");
+  webllm.prebuiltAppConfig.model_list.forEach((model, index) => {
+    let option = new Option(model.model_id, model.model_id);
+    modelSelect.add(option);
+  });
 
-    modelSelect.addEventListener('change', function() {
-        initializeChat(this.value);
-    });
+  modelSelect.addEventListener("change", function () {
+    initializeChat(this.value);
+  });
 }
 
 // Initialize the chat
 async function initializeChat(selectedModel) {
-    try {
-        const initProgressCallback = (report) => {
-            showProgress(report.text);
-        };
-        const engine = await webllm.CreateEngine(selectedModel, { initProgressCallback });
-        const chatUI = new ChatUI(engine);
-        chatUI.setupEventListeners();
-        clearProgress();
-    } catch (error) {
-        Boostlet.hint(`Error: ${error.message}`, 3000);
-    }
+  try {
+    const initProgressCallback = (report) => {
+      showProgress(report.text);
+    };
+    const engine = await webllm.CreateEngine(selectedModel, {
+      initProgressCallback,
+    });
+    window.engine = engine;
+
+    const chatUI = new ChatUI(engine);
+    chatUI.setupEventListeners();
+    clearProgress();
+  } catch (error) {
+    Boostlet.hint(`Error: ${error.message}`, 8000);
+  }
 }
 
 function showProgress(text) {
-    let progressBox = document.getElementById('progress-box');
-    if (!progressBox) {
-        progressBox = document.createElement('div');
-        progressBox.id = 'progress-box';
-        progressBox.style = 'position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); background: rgba(0, 0, 0, 0.75); color: white; padding: 20px; border-radius: 10px; z-index: 1500;';
-        document.body.appendChild(progressBox);
-    }
-    progressBox.innerText = text;
+  let progressBox = document.getElementById("progress-box");
+  if (!progressBox) {
+    progressBox = document.createElement("div");
+    progressBox.id = "progress-box";
+    progressBox.style =
+      "position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); background: rgba(0, 0, 0, 0.75); color: white; padding: 20px; border-radius: 10px; z-index: 1500;";
+    document.body.appendChild(progressBox);
+  }
+  progressBox.innerText = text;
 }
 
 function clearProgress() {
-    const progressBox = document.getElementById('progress-box');
-    if (progressBox) {
-        document.body.removeChild(progressBox);
-    }
+  const progressBox = document.getElementById("progress-box");
+  if (progressBox) {
+    document.body.removeChild(progressBox);
+  }
 }
 
 class ChatUI {
   constructor(engine) {
-      this.engine = engine;
-      this.uiChat = document.getElementById("chatui-chat");
-      this.uiInput = document.getElementById("chatui-input");
-      this.uiSendBtn = document.getElementById("chatui-send-btn");
-      this.uiResetBtn = document.getElementById("chatui-reset-btn");
+    this.engine = engine;
+    this.uiChat = document.getElementById("chatui-chat");
+    this.uiInput = document.getElementById("chatui-input");
+    this.uiSendBtn = document.getElementById("chatui-send-btn");
+    this.uiResetBtn = document.getElementById("chatui-reset-btn");
   }
 
   setupEventListeners() {
-      this.uiSendBtn.addEventListener('click', () => this.sendChatMessage());
-      this.uiResetBtn.addEventListener('click', () => this.resetChat());
+    this.uiSendBtn.addEventListener("click", () => this.sendChatMessage());
+    this.uiResetBtn.addEventListener("click", () => this.resetChat());
   }
 
   sendChatMessage() {
     const message = this.uiInput.value.trim();
     if (!message) return;
-    this.appendMessage('right-msg', message, "User");
-    this.uiInput.value = '';
+    this.appendMessage("right-msg", message, "User");
+    this.uiInput.value = "";
     this.processChatMessage(message);
-}
+  }
 
   resetChat() {
-      this.uiChat.innerHTML = '';
+    this.uiChat.innerHTML = "";
   }
 
   appendMessage(className, text, role) {
     const chat = this.uiChat;
-    const messageWrapper = document.createElement('div');
-    messageWrapper.className = 'msg ' + className;
+    const messageWrapper = document.createElement("div");
+    messageWrapper.className = "msg " + className;
 
-    const imgSrc = (role === "User") ? "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" : "https://en.shiftdelete.net/wp-content/uploads/2023/06/chatbots-unleashed-utilizing-a-gpt-like-mlc-local-chatbot-in-macos-1.jpg";
+    const imgSrc =
+      role === "User"
+        ? "https://raw.githubusercontent.com/gaiborjosue/boostlet/webllm/gfx/user_avatar.png"
+        : "https://raw.githubusercontent.com/gaiborjosue/boostlet/webllm/gfx/llm_avatar.png";
     const profileImg = `<img src="${imgSrc}" alt="${role}" class="profile-image">`;
 
     messageWrapper.innerHTML = `
@@ -242,12 +265,17 @@ class ChatUI {
 
   async processChatMessage(message) {
     try {
-        const response = await this.engine.chat.completions.create({
-            messages: [{role: "user", content: message}]
-        });
-        this.appendMessage('left-msg', response.choices[0].message.content);
+      const response = await this.engine.chat.completions.create({
+        messages: [{
+            role: "system", "content": "You are a chatbot that will help users answer questions and your name is BoostBot. Start every message with 'BoostBot: ' to make it clear that you are a chatbot."
+        },{
+            role: "user", content: message
+        }]
+      });
+      this.appendMessage("left-msg", response.choices[0].message.content);
+      console.log(await engine.runtimeStatsText());
     } catch (error) {
-        Boostlet.hint(`Error: ${error.message}`, 3000);
+      Boostlet.hint(`Error: ${error.message}`, 8000);
     }
- }
-};
+  }
+}
