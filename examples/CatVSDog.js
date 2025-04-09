45ae9c8b4f9b4b351b8266667a06041e9e66ac11
Daniel Haehn <haehn@mpsych.org>
2025-04-09 19:51:25 -0400

    Upd.

--- examples/CatVSDog.js
+++ examples/CatVSDog.js
@@ -1,3 +1,4 @@
+
 script = document.createElement("script");
 script.type = "text/javascript";
 script.src = "https://boostlet.org/dist/boostlet.min.js";
@@ -13,30 +14,61 @@ function run() {
   Boostlet.init();
 
   // load TF.js
-  Boostlet.load_script('https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.17.0/dist/tf.min.js', function() {
+  Boostlet.load_script('https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.17.0/dist/tf.min.js', async function() {
 
-    // TF available
-    model = await tf.loadLayersModel("https://fly.cs.umb.edu/data/X/catdog/model.json");
-    
-    width = nv.volumes[0].dimsRAS[1]
-height = nv.volumes[0].dimsRAS[2]
+	  // TF available
+	  model = await tf.loadLayersModel("https://fly.cs.umb.edu/data/X/catdog/model.json");
+	    
+	  width = nv.volumes[0].dimsRAS[1]
+	  height = nv.volumes[0].dimsRAS[2]
 
-const imageData = new ImageData(new Uint8ClampedArray(nv.volumes[0].img), width, height);
+	  // GRAB DATA
+	  const imageData = new ImageData(new Uint8ClampedArray(nv.volumes[0].img), width, height);
 
-tensor = tf.browser.fromPixels(imageData).resizeNearestNeighbor([150, 150]) // resize
-  							.div(tf.scalar(255)) // normalize
-  							.expandDims();
+	  // RESHAPE
+	  tensor = tf.browser.fromPixels(imageData).resizeNearestNeighbor([150, 150]) 
+	  							.div(tf.scalar(255)) // normalize
+	  							.expandDims();
 
-prediction = await model.predict(tensor).dataSync();
+	  // INFERENCE
+	  prediction = await model.predict(tensor).dataSync();
 
-console.log(prediction);
+
+	  if (prediction < 0.5) {
+
+	  	// its a cat
+	  	text = "MEOW " + Math.round((1-prediction)*100,2) + "% !";
+
+	  } else {
+
+	  	// WOOF
+	  	text = "WOOF " + Math.round(prediction*100,2) + "% !";
+
+	  }
+	 
+
+		const topText = document.createElement('div');
+		topText.innerText = text;
+		topText.style.position = 'absolute';
+		topText.style.top = '0';
+		topText.style.left = '0';
+		topText.style.width = '100%';
+		topText.style.textAlign = 'center';
+		topText.style.padding = '40px';
+		topText.style.fontSize = '48px';
+		topText.style.fontWeight = 'bold';
+		topText.style.fontFamily = 'sans-serif';
+		topText.style.zIndex = 31337;
+		document.body.appendChild(topText);
 
 
+	  console.log(prediction);
 
   });
 
 
 }
+
 
 
 
